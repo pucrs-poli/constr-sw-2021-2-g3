7509ac6501662a51597305f8bfebb2a2b14c3242
almeida_gabriel <almeida_gabriel@sicredi.com.br>
2021-11-21 16:51:19 -0300

    refatora serviço de usuários

--- users/src/main/java/com/construcao/software/users/controller/UsuarioController.java
+++ users/src/main/java/com/construcao/software/users/controller/UsuarioController.java
@@ -1,11 +1,11 @@
 package com.construcao.software.users.controller;
 
-import com.construcao.software.users.dto.PapelDTO;
 import com.construcao.software.users.dto.UsuarioDTO;
 import com.construcao.software.users.model.Papel;
 import com.construcao.software.users.model.Usuario;
 import com.construcao.software.users.repository.PapelRepository;
 import com.construcao.software.users.repository.UsuarioRepository;
+import com.construcao.software.users.service.UsuarioService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.util.UriComponentsBuilder;
@@ -18,12 +18,11 @@ import java.util.stream.Collectors;
 @RestController
 @RequestMapping("/usuarios")
 public class UsuarioController {
-    private final UsuarioRepository usuarioRepository;
-    private final PapelRepository papelRepository;
 
-    public UsuarioController(UsuarioRepository usuarioRepository, PapelRepository papelRepository) {
-        this.usuarioRepository = usuarioRepository;
-        this.papelRepository = papelRepository;
+    private final UsuarioService usuarioService;
+
+    public UsuarioController(UsuarioService usuarioService) {
+        this.usuarioService = usuarioService;
     }
 
     @GetMapping
@@ -31,115 +30,46 @@ public class UsuarioController {
                                                               @RequestParam(required = false) String login,
                                                               @RequestParam(required = false) String email,
                                                               @RequestParam(required = false) String inicioNome) {
-        List<UsuarioDTO> usuarios;
-        if (matricula == null && login == null && email == null && inicioNome == null) {
-            usuarios = converterUsuariosParaDTO(usuarioRepository.findAll());
-        } else {
-            usuarios = converterUsuariosParaDTO(usuarioRepository.findAllByMatriculaOrLoginOrEmailOrNomeStartsWith(matricula, login, email, inicioNome));
-        }
+        var usuarios = usuarioService.recuperarUsuarios(matricula, login, email, inicioNome);
         return ResponseEntity.ok(usuarios);
-    }
-
-    private List<UsuarioDTO> converterUsuariosParaDTO(List<Usuario> usuarios) {
-        return usuarios.stream()
-                .map(usuario -> {
-                    var papeis = usuario.getPapeis()
-                            .stream()
-                            .map(papel -> new PapelDTO(papel.getNome()))
-                            .collect(Collectors.toList());
-
-                    return new UsuarioDTO(usuario.getEmail(), usuario.getNome(), usuario.getLogin(), papeis, usuario.getMatricula());
-                })
-                .collect(Collectors.toList());
     }
 
     @GetMapping("/{id}")
     public ResponseEntity<Usuario> recuperarUsuariosPorId(@PathVariable String id) {
-        var result = usuarioRepository.findById(id);
-        return result.map(ResponseEntity::ok).orElseGet(() -> ResponseEntity.notFound().build());
+        return usuarioService.recuperarUsuariosPorId(id)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
     }
 
     @PostMapping
-    public ResponseEntity<?> criarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO,
+    public ResponseEntity<Usuario> criarUsuario(@Valid @RequestBody UsuarioDTO usuarioDTO,
                                           UriComponentsBuilder b) {
-
-        var papeis = buscaPapeis(usuarioDTO);
-
-        var usuario = new Usuario(usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getLogin(), papeis, usuarioDTO.getMatricula());
-        var salvo = usuarioRepository.save(usuario);
+        var salvo = usuarioService.criarUsuario(usuarioDTO);
         var uriComponents = b.path("/usuarios/{id}").buildAndExpand(salvo.getId());
         return ResponseEntity.created(uriComponents.toUri()).body(salvo);
     }
 
     @DeleteMapping("/{id}")
-    public ResponseEntity<?> deletarUsuario(@PathVariable String id) {
-        var usuario = usuarioRepository.findById(id);
-        if (usuario.isEmpty()) {
+    public ResponseEntity<Void> deletarUsuario(@PathVariable String id) {
+        try {
+            usuarioService.deletarUsuario(id);
+        } catch(IllegalArgumentException err) {
             return ResponseEntity.notFound().build();
         }
-        usuarioRepository.deleteById(id);
         return ResponseEntity.noContent().build();
     }
 
     @PutMapping("/{id}")
-    public ResponseEntity<?> alterarTodoUsuario(@PathVariable String id,
+    public ResponseEntity<Usuario> alterarTodoUsuario(@PathVariable String id,
                                           @RequestBody UsuarioDTO usuarioDTO) {
-        var usuario = usuarioRepository.findById(id);
-
-        if (usuario.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        var papeis = buscaPapeis(usuarioDTO);
-
-        var entidade = new Usuario(id, usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getLogin(), papeis, usuarioDTO.getMatricula());
-        var entidadeSalva = usuarioRepository.save(entidade);
-
-        return ResponseEntity.ok().body(entidadeSalva);
+        var usuario = usuarioService.editarTodoUsuario(id, usuarioDTO);
+        return ResponseEntity.ok().body(usuario);
     }
 
     @PatchMapping("/{id}")
-    public ResponseEntity<?> alterarUsuario(@PathVariable String id,
+    public ResponseEntity<Usuario> alterarUsuario(@PathVariable String id,
                                             @RequestBody UsuarioDTO usuarioDTO) {
-        var usuario = usuarioRepository.findById(id);
-        if (usuario.isEmpty()) {
-            return ResponseEntity.notFound().build();
-        }
-
-        var usuarioAlterado = usuario.get();
-
-        List<Papel> papeis;
-        if (usuarioDTO.getPapeis() != null) {
-            papeis = buscaPapeis(usuarioDTO);
-            usuarioAlterado.setPapeis(papeis);
-        }
-
-        if (usuarioDTO.getEmail() != null) {
-            usuarioAlterado.setEmail(usuarioDTO.getEmail());
-        }
-
-        if (usuarioDTO.getNome() != null) {
-            usuarioAlterado.setNome(usuarioDTO.getNome());
-        }
-
-        if (usuarioDTO.getMatricula() != null) {
-            usuarioAlterado.setMatricula(usuarioDTO.getMatricula());
-        }
-
-        if (usuarioDTO.getLogin() != null) {
-            usuarioAlterado.setLogin(usuarioDTO.getLogin());
-        }
-
-        var entidadeSalva = usuarioRepository.save(usuarioAlterado);
+        var entidadeSalva = usuarioService.editarUsuario(id, usuarioDTO);
         return ResponseEntity.ok().body(entidadeSalva);
-    }
-
-    private List<Papel> buscaPapeis(@RequestBody UsuarioDTO usuarioDTO) {
-        return usuarioDTO.getPapeis()
-                .stream()
-                .map(it -> papelRepository.findByNome(it.getNome()))
-                .filter(Optional::isPresent)
-                .map(Optional::get)
-                .collect(Collectors.toList());
     }
 }
---
+++ users/src/main/java/com/construcao/software/users/service/UsuarioService.java
@@ -0,0 +1,121 @@
+package com.construcao.software.users.service;
+
+import com.construcao.software.users.dto.PapelDTO;
+import com.construcao.software.users.dto.UsuarioDTO;
+import com.construcao.software.users.model.Papel;
+import com.construcao.software.users.model.Usuario;
+import com.construcao.software.users.repository.PapelRepository;
+import com.construcao.software.users.repository.UsuarioRepository;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Service
+public class UsuarioService {
+
+    private final UsuarioRepository usuarioRepository;
+    private final PapelRepository papelRepository;
+
+    public UsuarioService(UsuarioRepository usuarioRepository, PapelRepository papelRepository) {
+        this.usuarioRepository = usuarioRepository;
+        this.papelRepository = papelRepository;
+    }
+
+    public List<UsuarioDTO> recuperarUsuarios(String matricula, String login, String email, String inicioNome) {
+        List<UsuarioDTO> usuarios;
+        if (matricula == null && login == null && email == null && inicioNome == null) {
+            usuarios = this.converterUsuariosParaDTO(usuarioRepository.findAll());
+        } else {
+            usuarios = this.converterUsuariosParaDTO(usuarioRepository.findAllByMatriculaOrLoginOrEmailOrNomeStartsWith(matricula, login, email, inicioNome));
+        }
+
+        return usuarios;
+    }
+
+    private List<UsuarioDTO> converterUsuariosParaDTO(List<Usuario> usuarios) {
+        return usuarios.stream()
+                .map(usuario -> {
+                    var papeis = usuario.getPapeis()
+                            .stream()
+                            .map(papel -> new PapelDTO(papel.getNome()))
+                            .collect(Collectors.toList());
+
+                    return new UsuarioDTO(usuario.getEmail(), usuario.getNome(), usuario.getLogin(), papeis, usuario.getMatricula());
+                })
+                .collect(Collectors.toList());
+    }
+
+    public Optional<Usuario> recuperarUsuariosPorId(String id) {
+        return usuarioRepository.findById(id);
+    }
+
+    public Usuario criarUsuario(UsuarioDTO usuarioDTO) {
+        var papeis = buscaPapeis(usuarioDTO);
+        var usuario = new Usuario(usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getLogin(), papeis, usuarioDTO.getMatricula());
+        return usuarioRepository.save(usuario);
+    }
+
+    private List<Papel> buscaPapeis(UsuarioDTO usuarioDTO) {
+        return usuarioDTO.getPapeis()
+                .stream()
+                .map(it -> papelRepository.findByNome(it.getNome()))
+                .filter(Optional::isPresent)
+                .map(Optional::get)
+                .collect(Collectors.toList());
+    }
+
+    public void deletarUsuario(String id) {
+        var usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()) {
+            throw new IllegalArgumentException("Usuário não encontrado.");
+        }
+        usuarioRepository.deleteById(id);
+    }
+
+    public Usuario editarTodoUsuario(String id, UsuarioDTO usuarioDTO) {
+        var usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()) {
+            throw new IllegalArgumentException("Usuário não encontrado.");
+        }
+
+        var papeis = buscaPapeis(usuarioDTO);
+        var entidade = new Usuario(id, usuarioDTO.getNome(), usuarioDTO.getEmail(), usuarioDTO.getLogin(), papeis, usuarioDTO.getMatricula());
+        return usuarioRepository.save(entidade);
+    }
+
+    public Usuario editarUsuario(String id, UsuarioDTO usuarioDTO) {
+        var usuario = usuarioRepository.findById(id);
+        if (usuario.isEmpty()) {
+            throw new IllegalArgumentException("Usuário não encontrado.");
+        }
+
+        var usuarioAlterado = usuario.get();
+
+        List<Papel> papeis;
+        if (usuarioDTO.getPapeis() != null) {
+            papeis = buscaPapeis(usuarioDTO);
+            usuarioAlterado.setPapeis(papeis);
+        }
+
+        if (usuarioDTO.getEmail() != null) {
+            usuarioAlterado.setEmail(usuarioDTO.getEmail());
+        }
+
+        if (usuarioDTO.getNome() != null) {
+            usuarioAlterado.setNome(usuarioDTO.getNome());
+        }
+
+        if (usuarioDTO.getMatricula() != null) {
+            usuarioAlterado.setMatricula(usuarioDTO.getMatricula());
+        }
+
+        if (usuarioDTO.getLogin() != null) {
+            usuarioAlterado.setLogin(usuarioDTO.getLogin());
+        }
+
+        return usuarioRepository.save(usuarioAlterado);
+    }
+}
